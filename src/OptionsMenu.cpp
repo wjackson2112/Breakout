4117275cde4a5bccb3cb8c20219e6783404f83f2
Will Jackson <wjackson2112@gmail.com>
2016-09-11 11:38:57 -0600

    Add Volume Label and Simplify Options Menu

--- src/OptionsMenu.cpp
+++ src/OptionsMenu.cpp
@@ -12,6 +12,6 @@
 OptionsMenu::OptionsMenu(AssetFactory* assetFactory, Options* options)
 	: Menu(X_OFFSET, Y_OFFSET, NUM_ROWS, ROW_HEIGHT, ROW_PADDING, NUM_COLS, COL_WIDTH, COL_PADDING)
 {
-	this->add_menu_item(new Button("./png/VideoOptions.png", assetFactory, options, VIDEO_OPTIONS), 0, 0);
-	this->add_menu_item(new Button("./png/AudioOptions.png", assetFactory, options, AUDIO_OPTIONS), 1, 0);
+	this->add_menu_item(new Label("./png/Volume.png", assetFactory, options), 0, 0);
+	this->add_menu_item(new Slider(assetFactory, options, "sound.volume", 0, 100, 0), 0, 1);
 }
